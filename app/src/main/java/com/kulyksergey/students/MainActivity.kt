9e41e58b06408336a8895306d061d6273adb6626
Artem <moorlotalhazred@gmail.com>
2021-09-29 15:04:30 +0300

    Add my name #19

---
+++ app/src/main/java/com/kulyksergey/students/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kulyksergey.students
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.lang.StringBuilder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
